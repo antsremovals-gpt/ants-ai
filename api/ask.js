c479d97f4a9eaf73a0540f3897fd2dff03d98990
antsremovals-gpt <oviidiu.surdu@gmail.com>
2025-09-25 11:18:12 +0100

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,7 +1,3 @@
-Ai vercel 
-
-
-
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
